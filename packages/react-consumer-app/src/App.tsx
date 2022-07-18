9281377f0a75dd7e2048f4530a2954bc0ad6cdff
neni <Nemanja.Ninkovic@zuehlke.com>
2022-07-18 11:38:30 +0800

    chore: fixes react consumer app to import the button component

--- packages/react-consumer-app/src/App.tsx
+++ packages/react-consumer-app/src/App.tsx
@@ -1,28 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {MyComponent} from '@company/component-library-react'
+import {ComponentButton} from '@company/component-library-react'
 
 function App() {
   return (
     <>
-      <MyComponent first={'The'} middle={'Amazing'} last={'Devs!'}></MyComponent>
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ComponentButton text={'Hello React'} type={'primary'}></ComponentButton>
     </>
   );
 }
